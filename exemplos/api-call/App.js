f93462e1da7ec14e223eee45269584f050eca72a
fmotamedeiros <flavio.medeiros@ifal.edu.br>
2021-10-24 11:05:21 -0300

    Adicionando exemplo de Flatlist com API call.

---
+++ exemplos/api-call/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View, StyleSheet, StatusBar } from 'react-native';
+
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+     try {
+      const response = await fetch('https://reactnative.dev/movies.json');
+      const json = await response.json();
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const Item = ({ title, releaseYear }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+      <Text>{releaseYear}</Text>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Item title={item.title} releaseYear={item.releaseYear} />
+          )}
+        />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#85C1E9',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
